262b832043ee762b4e3630d18626ad37258c8cec
marcschepp <marcphilippschepp@gmail.com>
2022-05-22 17:54:31 +0200

    created 5 new components to showcase basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,23 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { LendComponent } from './lend/lend.component';
+import { RentComponent } from './rent/rent.component';
+import { UserhomeComponent } from './userhome/userhome.component';
+import { UserComponent } from './user/user.component';
+import { CurrentrentComponent } from './currentrent/currentrent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    HomeComponent
+    HomeComponent,
+    LendComponent,
+    RentComponent,
+    UserhomeComponent,
+    UserComponent,
+    CurrentrentComponent
   ],
   imports: [
     BrowserModule,
